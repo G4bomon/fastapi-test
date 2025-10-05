eac3c80388dd8e5d4e05de8842e7e945c367d935
G4bomon <gabrielmontesbrea@gmail.com>
2025-10-05 19:31:05 -0400

    conceptos basicos

---
+++ main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+from routes1 import router as router1  # Importa el router
+from routes2 import router as router2  # Importa el router
+
+app = FastAPI()
+
+# Incluye las rutas del router
+app.include_router(router1)
+app.include_router(router2)
