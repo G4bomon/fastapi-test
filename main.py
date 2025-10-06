edc4162339d0313aa2887c22c24679fcd7739499
G4bomon <126829381+G4bomon@users.noreply.github.com>
2025-10-06 16:32:19 -0400

    agregar route3.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from routes1 import router as router1  # Importa el router
 from routes2 import router as router2  # Importa el router
+from routes3 import router as router3  # Importa el router
 
 app = FastAPI()
 
 # Incluye las rutas del router
 app.include_router(router1)
 app.include_router(router2)
+app.include_router(router3)
